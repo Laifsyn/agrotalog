485d9379bc69e80e4dfeac81c776e777feca0ef8
Laifsyn <99366187+Laifsyn@users.noreply.github.com>
2025-12-14 14:01:50 -0500

    Make selection update fields

--- ProyFinalAgropecuariaNET6/Form6.Designer.cs
+++ ProyFinalAgropecuariaNET6/Form6.Designer.cs
@@ -43,10 +43,12 @@
             dgvInventario.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
             dgvInventario.Size = new Size(453, 300);
             dgvInventario.TabIndex = 0;
+            dgvInventario.CellClick += dgvInventario_CellClick;
+            dgvInventario.CellContentClick += dgvInventario_CellContentClick;
             // 
             // txtProductoId
             // 
-            txtProductoId.Location = new Point(557, 35);
+            txtProductoId.Location = new Point(572, 36);
             txtProductoId.Name = "txtProductoId";
             txtProductoId.Size = new Size(200, 23);
             txtProductoId.TabIndex = 1;
@@ -54,7 +56,7 @@
             // 
             // txtCantidad
             // 
-            txtCantidad.Location = new Point(557, 64);
+            txtCantidad.Location = new Point(572, 65);
             txtCantidad.Name = "txtCantidad";
             txtCantidad.Size = new Size(100, 23);
             txtCantidad.TabIndex = 2;
@@ -80,7 +82,7 @@
             // 
             lblTipo.Location = new Point(483, 97);
             lblTipo.Name = "lblTipo";
-            lblTipo.Size = new Size(60, 20);
+            lblTipo.Size = new Size(98, 20);
             lblTipo.TabIndex = 6;
             lblTipo.Text = "Stock Mínimo:";
             // 
@@ -104,7 +106,7 @@
             // 
             // txtStockMinimo
             // 
-            txtStockMinimo.Location = new Point(557, 93);
+            txtStockMinimo.Location = new Point(572, 94);
             txtStockMinimo.Name = "txtStockMinimo";
             txtStockMinimo.Size = new Size(100, 23);
             txtStockMinimo.TabIndex = 9;
